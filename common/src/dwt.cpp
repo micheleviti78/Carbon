29ee8dcec61153d56034139469fea7464d67a258
micheleviti78 <micheleviti78@gmail.com>
2022-02-28 22:28:55 +0100

    added dwt header and source file

---
+++ common/src/dwt.cpp
@@ -0,0 +1,27 @@
+/**
+ ******************************************************************************
+ * @file           dwt.cpp
+ * @author         Michele Viti <micheleviti78@gmail.com>
+ * @date           Feb. 2022
+ * @brief          DISCO-STM32H747 Data Watchpoint and Trace Unit, header file
+ ******************************************************************************
+ * @attention
+ * Copyright (c) 2022 Michele Viti.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+
+#include<dwt.hpp>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif

e6223649e7713d1920d922297b2e6d275201c400
micheleviti78 <micheleviti78@gmail.com>
2022-06-22 20:30:08 +0200

    removed unused format script

--- format.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-find CM4/ -regex '.*\.\(cpp\|hpp\|cu\|c\|h\)' -exec clang-format -style=file -i {} \;
-find CM7/ -regex '.*\.\(cpp\|hpp\|cu\|c\|h\)' -exec clang-format -style=file -i {} \;
-find common/ -regex '.*\.\(cpp\|hpp\|cu\|c\|h\)' -exec clang-format -style=file -i {} \;
-

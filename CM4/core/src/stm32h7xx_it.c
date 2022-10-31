ed759f26f4c3bbe8493781b002f59acd3662d144
micheleviti78 <micheleviti78@gmail.com>
2022-10-31 15:55:54 +0100

    * FreeRTOS configured for CM4 * hsem handler implemented for CM4

--- CM4/core/src/stm32h7xx_it.c
+++ CM4/core/src/stm32h7xx_it.c
@@ -21,6 +21,7 @@
 #include <main.hpp>
 
 void carbon_hw_us_systime_tim_isr(void);
+void hsem_isr(void);
 
 /******************************************************************************/
 /*           Cortex Processor Interruption and Exception Handlers          */
@@ -66,27 +67,18 @@ void UsageFault_Handler(void) {
 }
 
 /**
- * @brief This function handles System service call via SWI instruction.
- */
-void SVC_Handler(void) {}
-
-/**
  * @brief This function handles Debug monitor.
  */
 void DebugMon_Handler(void) {}
-
-/**
- * @brief This function handles Pendable request for system service.
- */
-void PendSV_Handler(void) {}
-
-/**
- * @brief This function handles System tick timer.
- */
-void SysTick_Handler(void) {}
 
 /**
  * @brief This function handles TIM5.
  */
 
 void TIM5_IRQHandler(void) { carbon_hw_us_systime_tim_isr(); }
+
+/**
+ * @brief This function handles HSEM1.
+ */
+
+void HSEM2_IRQHandler(void) { hsem_isr(); }

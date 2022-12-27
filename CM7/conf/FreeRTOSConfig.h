0f5278252fe5168eea14e89f34e6f10a5b9ab8c7
micheleviti78 <micheleviti78@gmail.com>
2022-12-27 12:04:27 +0100

    FreeRTOS configuration

--- CM7/conf/FreeRTOSConfig.h
+++ CM7/conf/FreeRTOSConfig.h
@@ -55,7 +55,7 @@ extern uint32_t SystemCoreClock;
  * configMAX_PRIORITIES to 56
  *
  */
-/* #define configUSE_PORT_OPTIMISED_TASK_SELECTION	0*/
+#define configUSE_PORT_OPTIMISED_TASK_SELECTION	1
 /* #define configMAX_PRIORITIES					( 56 ) */
 #define configUSE_PREEMPTION 1
 #define configUSE_IDLE_HOOK 0
@@ -66,16 +66,17 @@ extern uint32_t SystemCoreClock;
 #define configCPU_CLOCK_HZ (SystemCoreClock)
 #define configTICK_RATE_HZ ((TickType_t)1000)
 #define configMINIMAL_STACK_SIZE ((uint16_t)128)
-#define configTOTAL_HEAP_SIZE ((size_t)(30 * 1024))
+#define configTOTAL_HEAP_SIZE ((size_t)(64 * 1024))
 #define configMAX_TASK_NAME_LEN (16)
 #define configUSE_TRACE_FACILITY 1
+#define configUSE_STATS_FORMATTING_FUNCTIONS 1
 #define configUSE_16_BIT_TICKS 0
 #define configIDLE_SHOULD_YIELD 1
 #define configUSE_MUTEXES 1
 #define configQUEUE_REGISTRY_SIZE 8
 #define configCHECK_FOR_STACK_OVERFLOW 2
 #define configUSE_RECURSIVE_MUTEXES 1
-#define configUSE_MALLOC_FAILED_HOOK 0
+#define configUSE_MALLOC_FAILED_HOOK 1
 #define configUSE_APPLICATION_TASK_TAG 0
 #define configUSE_COUNTING_SEMAPHORES 1
 #define configGENERATE_RUN_TIME_STATS 1
@@ -88,7 +89,7 @@ extern uint32_t SystemCoreClock;
 
 /* Software timer definitions. */
 #define configUSE_TIMERS 0
-#define configTIMER_TASK_PRIORITY (2)
+#define configTIMER_TASK_PRIORITY 2
 #define configTIMER_QUEUE_LENGTH 10
 #define configTIMER_TASK_STACK_DEPTH (configMINIMAL_STACK_SIZE * 2)
 

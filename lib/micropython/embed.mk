fefe4e04050a5967d0f3e0141931c05a185569ac
micheleviti78 <micheleviti78@gmail.com>
2024-01-28 18:23:13 +0100

    complete building micropython runs from cmake

--- lib/micropython/embed.mk
+++ lib/micropython/embed.mk
@@ -4,7 +4,11 @@
 # 
 # This file is intended to be included by a Makefile in a custom project.
 
-MICROPYTHON_TOP = micropython
+ROOT_LIB = $(ROOT_PROJECT)/lib
+
+ROOT_MICROPYTHON = $(ROOT_LIB)/micropython/
+
+MICROPYTHON_TOP = $(ROOT_MICROPYTHON)/micropython
 
 # Set the build output directory for the generated files.
 BUILD = build-embed
@@ -13,16 +17,16 @@ BUILD = build-embed
 include $(MICROPYTHON_TOP)/py/mkenv.mk
 
 # Include py core make definitions.
-include $(TOP)/py/py.mk
+include $(MICROPYTHON_TOP)/py/py.mk
 
 # Set the location of the MicroPython embed port.
-MICROPYTHON_EMBED_PORT = port/
+MICROPYTHON_EMBED_PORT = $(ROOT_MICROPYTHON)/port/
 
 # Set default makefile-level MicroPython feature configurations.
 MICROPY_ROM_TEXT_COMPRESSION ?= 0
 
 # Set CFLAGS for the MicroPython build.
-CFLAGS += -I. -I$(TOP) -I$(BUILD) -I$(MICROPYTHON_EMBED_PORT) -I../freertos/include -I/home/michele/Software/Carbon/CM7/conf -I../freertos/portable/GCC/ARM_CM7/r0p1
+CFLAGS += -I$(ROOT_MICROPYTHON) -I$(MICROPYTHON_TOP) -I$(BUILD) -I$(MICROPYTHON_EMBED_PORT) -I$(ROOT_LIB)/freertos/include -I$(ROOT_LIB)/freertos/portable/GCC/ARM_CM7/r0p1 -I$(ROOT_PROJECT)/CM7/conf
 CFLAGS += -Wall -Werror -std=c99
 
 # Define the required generated header files.
@@ -44,7 +48,7 @@ clean-micropython-embed-package:
 	$(RM) -rf $(PACKAGE_DIR)
 
 PACKAGE_DIR ?= micropython_embed
-PACKAGE_DIR_LIST = $(addprefix $(PACKAGE_DIR)/,py extmod shared/runtime genhdr)
+PACKAGE_DIR_LIST = $(addprefix $(PACKAGE_DIR)/,py extmod shared/runtime genhdr port)
 
 .PHONY: micropython-embed-package
 micropython-embed-package: $(GENHDR_OUTPUT)
@@ -61,6 +65,7 @@ micropython-embed-package: $(GENHDR_OUTPUT)
 	$(Q)$(CP) $(TOP)/shared/runtime/gchelper_native.c $(PACKAGE_DIR)/shared/runtime
 	$(ECHO) "- genhdr"
 	$(Q)$(CP) $(GENHDR_OUTPUT) $(PACKAGE_DIR)/genhdr
+	$(Q)$(CP) $(TOP)/../port/*.[ch] $(PACKAGE_DIR)/port
 
 # Include remaining core make rules.
-include $(TOP)/py/mkrules.mk
+include $(MICROPYTHON_TOP)/py/mkrules.mk
